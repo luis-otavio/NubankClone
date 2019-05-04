2fd7e29d9b84c497321d34a52f1f22622fa5acb4
luis-otavio <luisotavio05@gmail.com>
2019-05-04 17:36:22 -0300

    Finalizando card principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 190.780,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Tranferência de R$ 780,65 recebida de Luis Otavio R. hoje às 13:00h
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
